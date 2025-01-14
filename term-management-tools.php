9196bf827b5ccb33b151e33577ce0d3ca3432a4f
Sepehr <45466228+realsepehrz@users.noreply.github.com>
2025-01-15 01:54:29 +0330

    Update term-management-tools.php
    Updated term-management-tools.php with new version and author
    
    - Increased version to 2.0.2.
    - Added myself to the list of authors.

--- term-management-tools.php
+++ term-management-tools.php
@@ -1,15 +1,22 @@
 <?php
+// CHANGED
+// If this file is called directly, abort.
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly.
+}
+
 /*
 Plugin Name: Term Management Tools
-Version: 2.0.1
+Version: 2.0.2
 Description: Allows you to merge terms, move terms between taxonomies, and set term parents, individually or in bulk. The "Change Taxonomy" option supports WPML-translated terms.
-Author: theMikeD, scribu
+Author: theMikeD, scribu, realsepehrz
 Author URI: https://www.codenamemiked.com
 Plugin URI: https://www.codenamemiked.com/plugins/term-management-tools/
 Text Domain: term-management-tools
 Domain Path: /lang
 Requires PHP: 7.1
 */
+
 
 define( 'CNMD_TMT_PLUGIN_URL_RELATIVE', basename( dirname( __FILE__ ) ) );
 define( 'CNMD_TMT_URL', plugin_dir_url( __FILE__ ) );
@@ -24,8 +31,9 @@ spl_autoload_register(
 		if ( ! preg_match( '/^CNMD\\\TMT/', $class ) ) {
 			return;
 		}
-
-		$filepath = str_replace( '\\', DIRECTORY_SEPARATOR, $class ) . '.php';
+		// CHANGED
+		// $filepath = str_replace( '\\', DIRECTORY_SEPARATOR, $class ) . '.php';
+		$filepath = str_replace( '\\', DIRECTORY_SEPARATOR, esc_attr( $class ) ) . '.php';
 		$filename = plugin_dir_path( __FILE__ ) . '/classes/class-' . basename( $filepath );
 		if ( file_exists( $filename ) ) {
 			include_once $filename;
