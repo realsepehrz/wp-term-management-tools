efc6bab82e70eebfae7766927939fb58774a338c
Sepehr <45466228+realsepehrz@users.noreply.github.com>
2025-01-15 01:57:14 +0330

    Update script.js
    same as the /assets/script.js

--- assets/src/script.js
+++ assets/src/script.js
@@ -1,52 +1,48 @@
-jQuery(
-	function($) {
-		var actions = [];
+jQuery(function($) {
+    var actions = [];
 
-		$.each(
-			tmtL10n,
-			function(key, title) {
-				actions.unshift(
-					{
-						action: 'bulk_' + key,
-						name: title,
-						el: $( '#tmt-input-' + key )
-					}
-				);
-			}
-		);
+    // Iterate over tmtL10n, excluding the 'refreshText' key
+    $.each(tmtL10n.actions, function(key, title) {
+        actions.unshift({
+            action: 'bulk_' + key,
+            name: title,
+            el: $('#tmt-input-' + key)
+        });
+    });
 
-		$( '.actions select' )
-		.each(
-			function() {
-				var $option = $( this ).find( 'option:first' );
+    // Add actions to the bulk actions dropdown
+    $('.actions select').each(function() {
+        var $option = $(this).find('option:first');
 
-				$.each(
-					actions,
-					function(i, actionObj) {
-						$option.after( $( '<option>', {value: actionObj.action, html: actionObj.name} ) );
-					}
-				);
-			}
-		)
-		.change(
-			function() {
-				var $select = $( this );
+        $.each(actions, function(i, actionObj) {
+            $option.after(
+                $('<option>', {
+                    value: $('<div>').text(actionObj.action).html(),
+                    html: $('<div>').text(actionObj.name).html()
+                })
+            );
+        });
+    }).change(function() {
+        var $select = $(this);
 
-				$.each(
-					actions,
-					function(i, actionObj) {
-						if ( $select.val() === actionObj.action ) {
-							actionObj.el
-							.insertAfter( $select )
-							.css( 'display', 'inline' )
-							.find( ':input' ).focus();
-						} else {
-							actionObj.el
-							.css( 'display', 'none' );
-						}
-					}
-				);
-			}
-		);
-	}
-);
+        $.each(actions, function(i, actionObj) {
+            if ($select.val() === actionObj.action) {
+                actionObj.el
+                    .insertAfter($select)
+                    .css('display', 'inline')
+                    .find(':input').focus();
+            } else {
+                actionObj.el.css('display', 'none');
+            }
+        });
+    });
+});
+
+jQuery(document).ready(function($) {
+    // Add the refresh text to the page
+    $('.alignleft.actions.bulkactions').after(
+        '<span style="height:100%; display:inline-flex; align-items:center;" class="rfrsh-txt">' +
+        tmtL10n.refreshText +
+        '</span>'
+    );
+});
